5e7ec996190e3b5acfbe52bd86760cf6c3e3e297
Sarweshkumar C R <sarweshkumar47@gmail.com>
2016-01-20 14:52:20 +0530

    Update adsb_run.sh

--- adsb_run.sh
+++ adsb_run.sh
@@ -3,5 +3,6 @@
 echo "RTL SDR is Up....................!!!"
 /rtlsdr/dump1090/./dump1090 --interactive
 
-#/rtlsdr/dump1090/./dump1090 --interactive --net --net-ro-port 31001 --net-http-port 8080
+#Embedded HTTP server which displays the currently detected aircrafts on Google Map locally
+#/rtlsdr/dump1090/./dump1090 --interactive --net --net-ro-port 31001 --net-http-port 8080 
 
